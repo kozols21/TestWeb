b0075e0541e44580a38ccf13b271ec2a5cc3f644
kihakik <kihakik@DESKTOP-AUP8V0E>
2021-02-24 11:40:57 +0200

    6. commit

--- main.js
+++ main.js
@@ -4,10 +4,13 @@
  * and open the template in the editor.
  */
 
+var executed = false;
 
 function f(){
-
+if (!executed){
     let x=document.createElement("button");
     x.innerHTML="poga";
     document.getElementById("divtags").appendChild(x);
+    executed=true;
+}
 }
