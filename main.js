9207521df5476ca8c632b54c377aca731c56c53e
kihakik <kihakik@DESKTOP-AUP8V0E>
2021-02-24 12:31:54 +0200

    6. commit

--- main.js
+++ main.js
@@ -8,9 +8,21 @@ var executed = false;
 
 function f(){
 if (!executed){
-    let x=document.createElement("button");
-    x.innerHTML="poga";
-    document.getElementById("divtags").appendChild(x);
+   console.log(document.getElementById("nosaukums").value);
+    let n = document.getElementById("nosaukums").value;
+    
+    
+    for(let a =0;a<n;a++){
+        
+         document.getElementById("divtags").appendChild(document.createElement("br"));
+        for(let b =0;b<n;b++){
+            let x=document.createElement("button");
+            x.innerHTML=(a)*(n)+(b+1);
+            document.getElementById("divtags").appendChild(x);
+           
+        }
+    }
+    
     executed=true;
 }
 }
