46fba17dc0bd9d6bd265c61fc7346e568578731a
kihakik <kihakik@DESKTOP-AUP8V0E>
2021-03-03 12:10:01 +0200

    6. commit

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function f(){
     var m = 0;
 if (m!==document.getElementById("nosaukums").value){
     document.getElementById("divtags").innerHTML = '';
-   console.log(document.getElementById("nosaukums").value);
+  // console.log(document.getElementById("nosaukums").value);
     let n = document.getElementById("nosaukums").value;
     var m =n;
     
@@ -19,7 +19,11 @@ if (m!==document.getElementById("nosaukums").value){
          document.getElementById("divtags").appendChild(document.createElement("br"));
         for(let b =0;b<n;b++){
             let x=document.createElement("button");
-            x.innerHTML=(a)*(n)+(b+1);
+           // x.innerHTML=(a)*(n)+(b+1);
+            let y=document.createElement("img");
+          
+            y.src="b1.jpg";
+            x.appendChild(y);
             document.getElementById("divtags").appendChild(x);
            
         }
@@ -27,4 +31,16 @@ if (m!==document.getElementById("nosaukums").value){
     
    
 }
+}
+function y(z){
+    //ja z ir lielaks par noteiktu vertibu aptur ciklu
+    //PS ieprieks pievieno mainigo lai skaititu 0 vertibas atseviski
+    //random skaitlis
+    //pec rand nosaka bildes nummuru nr
+    //ja sk masiva pie nr mazak par 2, tad
+        //tad sk masiva pie nr++;
+        //bilzu mas pie z = nr
+        //y(z+1);
+    //ja sk masiva pie nr = 2, tad
+        //y(z);
 }
